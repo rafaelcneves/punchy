c29d300dac73596ca6f39d994b24eb211d8e0cce
Rafael César Neves <rafael.ceesar@gmail.com>
2012-06-09 13:53:35 -0300

    adicionando campo de user no punch

---
+++ db/migrate/20120609165102_add_user_to_punch.rb
@@ -0,0 +1,9 @@
+class AddUserToPunch < ActiveRecord::Migration
+  def self.up
+    add_column :punches, :created_by_id, :integer
+  end
+
+  def self.down
+    remove_column :punches, :created_by_id
+  end
+end
